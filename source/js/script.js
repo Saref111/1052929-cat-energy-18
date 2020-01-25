90341b799e7ddc24f81c065ac18c8718d4b07bf3
Saref111 <saref012@gmail.com>
2020-01-25 13:36:17 +0800

    JS fix

--- source/js/script.js
+++ source/js/script.js
@@ -8,4 +8,6 @@ menu.classList.add("main-nav__list--hidden");
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   menu.classList.toggle("main-nav__list--hidden");
+  button.classList.toggle("main-nav__toggle--opened");
+  button.classList.toggle("main-nav__toggle--closed");
 });
